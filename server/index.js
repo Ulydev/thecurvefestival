f42b6067dac2e2628a0ea8cb164c1702808ca42c
ulydev <ulydev@live.fr>
2020-04-04 18:58:58 +0200

    add websockets broadcasting on server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
+// create express app
 const express = require("express")
-const expressWS = require("express-ws")(express)
-const app = expressWS.app
+const app = express()
 const port = 3001
 
+// add websocket bindings
+const expressWS = require("express-ws")(app)
 const appWSServer = expressWS.getWss("/")
+
+// middleware
+const cors = require("cors")
+app.use(cors())
+
+app.get("/", (req, res) => res.send("Server is working"))
 
 app.ws("/", (ws, req) => {
     console.log("* socket connected")
 
     // broadcast
-    ws.onmessage = msg => {
-        console.log(msg.data)
-        appWSServer.clients.forEach(client => client.send(msg.data))
-    }
+    ws.on("message", msg => {
+        console.log("received message", msg)
+        appWSServer.clients.forEach(client => client.send(msg))
+    })
 })
 
 app.listen(port, () => console.log(`* app listening at http://localhost:${port}`))
