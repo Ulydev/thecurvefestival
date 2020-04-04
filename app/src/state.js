3a1ab5983c2f2ed0e7f7c6345cee677511d76b94
ulydev <ulydev@live.fr>
2020-04-04 18:58:19 +0200

    edit state to use reducer pattern

--- app/src/state.js
+++ app/src/state.js
@@ -1,9 +1,52 @@
-import { createGlobalState } from "react-hooks-global-state"
+import { createStore } from "react-hooks-global-state"
+
+import ClickEvent from "./interaction/ClickEvent"
+
+const reducer = (state, action) => {
+    // define actions
+    switch (action.type) {
+        case "ADD_EVENT": {
+            const event = action.event
+            switch (event.type) {
+                case ClickEvent.type: {
+                    return {
+                        ...state,
+                        clicks: [ ...state.clicks, event]
+                    }
+                }
+                default: {
+                    console.warn("(add) unknown event", event)
+                    return state
+                }
+            }
+        }
+        case "REMOVE_EVENT": {
+            const event = action.event
+            switch (event.type) {
+                case ClickEvent.type: {
+                    return {
+                        ...state,
+                        clicks: state.clicks.filter(clickEvent => clickEvent !== event)
+                    }
+                }
+                default: {
+                    console.warn("(remove) unknown event", event)
+                    return state
+                }
+            }
+        }
+        default: {
+            console.warn("unknown action", action)
+            return state;
+        }
+    }
+}
 
 const initialState = {
     // define initial state
+    clicks: []
 }
 
-const { useGlobalState } = createGlobalState(initialState)
+const { dispatch, useGlobalState } = createStore(reducer, initialState)
 
-export default useGlobalState
+export { dispatch, useGlobalState }
