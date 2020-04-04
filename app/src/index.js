07abede84149b8f46ad3bd33137811e037ebcf9c
ulydev <ulydev@live.fr>
2020-04-04 16:13:52 +0200

    add flex styles

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App"
 import "./styles/index.css"
 import "./styles/cursor.css"
 import "./styles/position.css"
+import "./styles/flex.css"
 // add global styles files here
 
 ReactDOM.render(
