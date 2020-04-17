c5886d8979b92cfb95c69519a29cabb641a4e210
ulydev <ulydev@live.fr>
2020-04-17 20:30:15 +0200

    add header

---
+++ app/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <a
+            id="header"
+            className="font-title text-gradient"
+            href="https://thecurvefestival.org"
+        >
+            The<br/>Curve
+        </a>
+    )
+}
+
+export default Header
