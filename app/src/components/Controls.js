5fa847f8aade6fb06769c0aa995e22d5bdc2e1b2
ulydev <ulydev@live.fr>
2020-04-17 20:31:53 +0200

    add stage minimal control

--- app/src/components/Controls.js
+++ app/src/components/Controls.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import StreamInformation from "./StreamInformation"
 import ViewersCount from "./ViewersCount"
+import StageSelectionMinimal from "./StageSelectionMinimal"
 import DonationForm from "./DonationForm"
 import CommentForm from "./CommentForm"
 import ReactionBoard from "./ReactionBoard"
@@ -11,8 +12,9 @@ import "./Controls.css"
 const Controls = () => {
     return (
         <div id="controls">
-            <StreamInformation />
-            <ViewersCount />
+            {/* <StreamInformation />
+            <ViewersCount /> */}
+            <StageSelectionMinimal />
             <DonationForm />
             <CommentForm />
             <ReactionBoard />
