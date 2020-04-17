b59113905523012771e5d63003dd8ecf5a126d99
ulydev <ulydev@live.fr>
2020-04-17 20:30:53 +0200

    add selection flow

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,13 +1,29 @@
 import React from "react"
 
+import { SwitchTransition, CSSTransition } from "react-transition-group"
+
+import Header from "./Header"
 import Controls from "./Controls"
 import FullscreenStream from "./FullscreenStream"
+import { useGlobalState } from "../state"
+
+import StageSelection from "./StageSelection"
 
 const App = () => {
+
+    const [stage] = useGlobalState("stage")
+
     return (
         <div id="app">
-            <Controls />
-            <FullscreenStream />
+            <Header />
+            { stage ?
+                <>
+                    <Controls />
+                    <FullscreenStream stage={stage} />
+                </>
+            :
+                <StageSelection />
+            }
         </div>
     )
 }
