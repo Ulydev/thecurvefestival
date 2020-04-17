87b7ac200dc8e41675459503463b2fa93905270f
ulydev <ulydev@live.fr>
2020-04-17 20:30:00 +0200

    import misc styles

--- app/src/index.js
+++ app/src/index.js
@@ -4,16 +4,20 @@ import App from "./components/App"
 
 import "./mobilecheck"
 
+import "./assets/fonts/lines-regular-webfont.ttf"
+
 import "./styles/index.css"
 import "./styles/cursor.css"
 import "./styles/position.css"
 import "./styles/flex.css"
 import "./styles/input.css"
 import "./styles/border.css"
+import "./styles/neon.css"
+import "./styles/variables.css"
+import "./styles/fonts.css"
+import "./styles/links.css"
+import "./styles/animations.css"
 // add global styles files here
-
-import { interactionController as interaction } from "./interaction/InteractionController"
-interaction.connect()
 
 ReactDOM.render(
     <React.StrictMode>
