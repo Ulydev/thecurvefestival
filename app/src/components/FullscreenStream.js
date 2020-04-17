081a169cc5f44c996bdc00be17853ab670bd18f3
ulydev <ulydev@live.fr>
2020-04-17 20:30:23 +0200

    use vimeo for fullscreenstream

--- app/src/components/FullscreenStream.js
+++ app/src/components/FullscreenStream.js
@@ -1,40 +1,34 @@
-import React from "react"
-import YouTube from "react-youtube"
+import React, { useMemo } from "react"
+
+import VimeoPlayer from "react-player/lib/players/Vimeo"
 
 import "./FullscreenStream.css"
 
 import offlineImage from "../assets/offline.png"
 
-const christianLofferStream = "OijrlItssOc"
+import config from "../interaction/config"
 
-const FullscreenStream = () => {
-
-    const opts = {
-		height: '100%',
-		width: '100%',
-		playerVars: {
-			autoplay: 1,
-            controls: 0,
-            disablekb: 1,
-			cc_load_policy: 0,
-			fs: 0,
-			iv_load_policy: 3,
-			modestbranding: 1,
-            rel: 0,
-            showinfo: 0 // NOTE: parameter is disabled (https://developers.google.com/youtube/player_parameters#release_notes_08_23_2018)
-		}
-	}
+const FullscreenStream = ({ stage }) => {
 
     return (
-        <div id="fullscreen-stream" style={{ backgroundColor: "black", opacity: 0.4 }} className="pointer-events-none">
+        /* <div id="fullscreen-stream" style={{ backgroundColor: "black", opacity: 0.4 }} className="pointer-events-none">
             <img id="offline-image" src={offlineImage} />
-        </div>
+        </div> */
         /* <YouTube
             id="fullscreen-stream"
             className="pointer-events-none"
             videoId={christianLofferStream} //TODO: change video id
             opts={opts}
         /> */
+        <div className='stream-player-wrapper'>
+            <VimeoPlayer
+                className='stream-player'
+                url={config.STAGES[stage - 1]}
+                width='100%'
+                height='100%'
+                playing
+            />
+        </div>
     )
 }
 
