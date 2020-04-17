42cc193062e1a20a74d35ab32ae14c6517aa6117
ulydev <ulydev@live.fr>
2020-04-17 20:31:31 +0200

    add stages id for videos

--- app/src/interaction/config.js
+++ app/src/interaction/config.js
@@ -1,5 +1,13 @@
-const config = {
-    WEBSOCKET_URI: "wss://api.thecurvefestival.org"
+let config = {
+    WEBSOCKET_URI: "wss://api.thecurvefestival.org",
+    STAGES_ID: [
+        "408864852",
+        "408866405",
+        "408866770"
+    ],
+    DONATION_URL: "https://tiltify.com/@thecurve/thecurvefestival/donate"
 }
+
+config.STAGES = config.STAGES_ID.map(id => `https://vimeo.com/${id}`)
 
 export default config
