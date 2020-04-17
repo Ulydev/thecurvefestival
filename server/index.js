254e2d203123f9f29a1c3c114292a490cbd383f8
ulydev <ulydev@live.fr>
2020-04-17 20:28:43 +0200

    fix server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 // create express app
-const express = require("express")
-const app = express()
+const express = require('express')
+const expressWs = require('@small-tech/express-ws')(express())
+const app = expressWs.app
 const port = 3001
-
-// add websocket bindings
-const expressWS = require("express-ws")(app)
-const appWSServer = expressWS.getWss("/")
 
 // middleware
 const cors = require("cors")
@@ -13,34 +10,13 @@ app.use(cors())
 
 app.get("/", (req, res) => res.send("Server is working"))
 
-const broadcast = msg => {
-    console.log("broadcasted message", msg)
-    appWSServer.clients.forEach(client => client.send(msg))
-}
-
-/*
-const broadcastViewersCount = () => {
-    broadcast(JSON.stringify({
-        type: "GLOBAL_STATE",
-        data: {
-            viewersCount: appWSServer.clients.size,
-            hostName: "OFFLINE" // TODO: dynamic host
-        }
-    }))
-}
-*/
-
-const handler = (ws, req) => {
+function handler(ws, req) {
     ws.room = this.setRoom(req)
     console.log(`* socket connected on ${ws.room}`)
 
-    // DEBUG: viewers count and stream information (now included directly in video)
-    /* broadcastViewersCount()
-    ws.on("close", broadcastViewersCount) */
-
     ws.on("message", msg => {
         console.log("received message", msg)
-        this.broadcast(ws, msg)
+        this.broadcast(ws, msg, { skipSelf: false })
     })
 }
 
