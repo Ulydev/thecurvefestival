2316aebf7f46c3bd844b706d55638ff04d1ecfcb
ulydev <ulydev@live.fr>
2020-04-18 14:22:14 +0200

    remove comments from vimeoplayer

--- app/src/components/FullscreenStream.js
+++ app/src/components/FullscreenStream.js
@@ -11,21 +11,19 @@ import config from "../interaction/config"
 const FullscreenStream = ({ stage }) => {
 
     return (
-        /* <div id="fullscreen-stream" style={{ backgroundColor: "black", opacity: 0.4 }} className="pointer-events-none">
-            <img id="offline-image" src={offlineImage} />
-        </div> */
-        /* <YouTube
-            id="fullscreen-stream"
-            className="pointer-events-none"
-            videoId={christianLofferStream} //TODO: change video id
-            opts={opts}
-        /> */
         <div className='stream-player-wrapper'>
             <VimeoPlayer
                 className='stream-player'
                 url={config.STAGES[stage - 1]}
                 width='100%'
                 height='100%'
+                config={{
+                    vimeo: {
+                        playerOptions: {
+                            quality: "1080p"
+                        }
+                    }
+                }}
                 playing
             />
         </div>
