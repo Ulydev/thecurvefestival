d7643ae3da62976cab09bd40a41dad1003d3d0a2
ulydev <ulydev@live.fr>
2020-04-18 14:21:58 +0200

    integrate donate box with viewer count

--- app/src/components/DonationForm.js
+++ app/src/components/DonationForm.js
@@ -4,52 +4,12 @@ import "./DonationForm.css"
 
 import config from "../interaction/config"
 
-/*
-const sendAmount = amount => {
-    console.log("sending amount", amount)
-}
-*/
-
 const DonationForm = () => {
 
-    const [amount, setAmount] = useState("")
-
-    /*
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-        if (amount.length === 0) return // prevent sending invalid amount TODO: check for negative or 0
-
-        sendAmount(amount)
-        setAmount("")
-    }
-    */
-
     return (
-        /*<div
-            id="donation-form"
-            className="z-inherit flex clip-children border-bottom-left border-top-right neon"
-        >
-            <form
-                className="flex flex-1"
-                onSubmit={handleSubmit}
-            >
-                <span className="input-prefix flex align-items-center">$</span>
-                <input
-                    className="flex-1" type="number" placeholder="1, 2, 5..." min="1" max="99999"
-                    onChange={e => setAmount(e.target.value)}
-                    value={amount}
-                />
-                <button
-                    type="submit"
-                >
-                    DONATE
-                </button>
-            </form>
-        </div>*/
         <a
             id="donation-button"
-            className="z-inherit button flex clip-children border-bottom-left border-top-right neon"
+            className="z-inherit button flex clip-children"
             href={config.DONATION_URL}
             target="_blank"
         >
